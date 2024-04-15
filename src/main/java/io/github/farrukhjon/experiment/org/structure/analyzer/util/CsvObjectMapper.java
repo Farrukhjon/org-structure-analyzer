a5792200b29b6eb10e7901f1acc506e4c5ea1b7a
Farrukhjon Sattorov <farrukhjon.sattorov@alertinnovation.com>
2024-04-15 13:47:21 +0500

    Added javaDocs

--- src/main/java/io/github/farrukhjon/experiment/org/structure/analyzer/util/CsvObjectMapper.java
+++ src/main/java/io/github/farrukhjon/experiment/org/structure/analyzer/util/CsvObjectMapper.java
@@ -5,16 +5,26 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
+ * Simple object mapper implementation of {@link ObjectMapper} from CSV file to Employee entity.
+ *
  * @author fsattorov
  */
 public class CsvObjectMapper implements ObjectMapper {
 
+    /**
+     * Maps content of the passed CSV file to Map of {@link Employee} by mapping employee IDs in keys.
+     *
+     * @param file CSV file.
+     * @return {@link Map} of employee IDs in keys to {@link Employee} in values.
+     */
     @Override
     public Map<Integer, Employee> toEmployees(final File file) {
+        Objects.requireNonNull(file, "Passed file for mapping must not be null!");
         try (final BufferedReader reader = new BufferedReader(new FileReader(file))) {
             return reader
                 .lines()
--- src/main/java/io/github/farrukhjon/experiment/org/structure/analyzer/util/ObjectMapper.java
+++ src/main/java/io/github/farrukhjon/experiment/org/structure/analyzer/util/ObjectMapper.java
@@ -5,10 +5,18 @@ import java.io.File;
 import java.util.Map;
 
 /**
+ * Simple object mapper interface.
+ *
  * @author fsattorov
  */
 public interface ObjectMapper {
 
+    /**
+     * Maps content of the passed file to Map of {@link Employee} by mapping employee IDs in keys.
+     *
+     * @param file a file.
+     * @return {@link Map} of employee IDs in keys to {@link Employee} in values.
+     */
     Map<Integer, Employee> toEmployees(File file);
 
 }
